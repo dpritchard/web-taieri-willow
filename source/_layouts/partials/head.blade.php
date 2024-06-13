9a41fd83f1f6c71bc9c9557017c670d454c2923b
Daniel Pritchard <daniel@pritchard.co>
2024-06-14 08:11:10 +1200

    Redirects...

--- source/_layouts/partials/head.blade.php
+++ source/_layouts/partials/head.blade.php
@@ -1,25 +1,7 @@
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
 
-        <meta property="og:title" content="{{ $page->title ? $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
-        <meta property="og:type" content="{{ $page->type ?? 'website' }}" />
-        <meta property="og:url" content="{{ $page->getUrl() }}"/>
-        <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
-
-        <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
-
-        <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="icon" href="{{ url('/favicon.ico') }}">
-        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
-
-        @if ($page->production)
-            <!-- Insert analytics code here -->
-        @endif
-
-        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
-        <link rel="stylesheet" href="{{ url('/assets/build/css/main.css') }}">
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    </head>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to {{ 'https://coastwillow.org.nz' . $page->getPath() }}</title>
+<meta http-equiv="refresh" content="0; URL={{ 'https://coastwillow.org.nz' . $page->getPath() }}">
+<link rel="canonical" href="{{ 'https://coastwillow.org.nz' . $page->getPath() }}">
