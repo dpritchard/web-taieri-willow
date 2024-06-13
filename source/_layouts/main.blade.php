244850254807be06ee4c1e956ccb7e72d42e7682
Daniel Pritchard <daniel@pritchard.co>
2024-06-14 08:23:29 +1200

    Friendlier Redirects...

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -4,36 +4,9 @@
 
     <body class="flex flex-col min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
         <header class="flex container flex-col max-w-6xl px-6 py-8">
-            <a href="{{ $page->baseUrl }}" title="{{ $page->siteName }} Home" class="inline-flex flex-1 items-center">
-                <!-- <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" /> -->
-                <h1 class="font-bold hover:text-emerald-700 my-0">{{ $page->siteName }}</h1>
-            </a>
+            <h1 class="font-bold hover:text-emerald-700 my-0">{{ $page->siteName }}</h1>
             <p class="text-sm text-slate-800">Formerly the Friends of the Taieri Willow</p>
-            <!-- @include('_layouts.partials.menu-toggle') -->
+            <p class="text-sm text-slate-800">We have a new domain name! Attempting to redirect you to <code>{{ 'www.coastwillow.org.nz' . $page->getPath() }}</code></p>
         </header>
-
-        <main role="main" class="flex flex-col lg:flex-row container max-w-6xl mx-auto px-6">
-            <nav id="nav-menu" 
-                 class="pb-4 mb-4 border-b-2 border-slate-200
-                        lg:flex lg:flex-col lg:border-r-2 lg:border-b-0 lg:pr-4 lg:mr-4 lg:pb-0">
-                @include('_layouts.partials.menu')
-            </nav>
-
-            <div class="">
-                 @yield('body')
-            </div>
-        </main>
-
-        <footer class="text-center text-sm mt-12 max-w-6xl mx-auto py-4" role="contentinfo">
-            <ul class="md:flex-row justify-center list-none">
-                <li>
-                    © FOTW {{ date("Y") }}
-                </li>
-            </ul>
-        </footer>
-
-        <script src="{{ url('/assets/build/js/main.js') }}"></script>
-
-        @stack('scripts')
     </body>
 </html>
--- source/_layouts/partials/head.blade.php
+++ source/_layouts/partials/head.blade.php
@@ -1,7 +1,13 @@
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	
+	<title>Redirecting to {{ 'https://coastwillow.org.nz' . $page->getPath() }}</title>
+	<meta http-equiv="refresh" content="1; URL={{ 'https://coastwillow.org.nz' . $page->getPath() }}">
+	<link rel="canonical" href="{{ 'https://coastwillow.org.nz' . $page->getPath() }}">
 
-<!DOCTYPE html>
-<meta charset="utf-8">
-<title>Redirecting to {{ 'https://coastwillow.org.nz' . $page->getPath() }}</title>
-<meta http-equiv="refresh" content="0; URL={{ 'https://coastwillow.org.nz' . $page->getPath() }}">
-<link rel="canonical" href="{{ 'https://coastwillow.org.nz' . $page->getPath() }}">
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
+	<link rel="stylesheet" href="{{ url('/assets/build/css/main.css') }}">
+	<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
